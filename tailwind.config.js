bde3f13ea587edacfc1516ca71d93930cebd421c
Vinicius <vmcmodesto@gmail.com>
2022-05-02 19:35:25 -0300

    mobile 414px

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,14 +5,29 @@ module.exports = {
   theme: {
     extend: {
 
+
+      screens: {
+        fhd: { 'max': '1920px' },
+        hd: { 'max': '1440px' },
+        xxl: { 'max': '1366px' },
+        xl: { 'max': '1024px' },
+        lg: { 'max': '976px' },
+        md: { 'max': '768px' },
+        sm: { 'max': '414px' },
+      },
+
       width: {
         '2px': '2px',
         '14%': '15%',
         '55%': '55%',
+        '68%': '68%',
+        '85%': '85%',
+        '95%': '95%',
       },
 
       height:{
-        '136%': '136px'
+        '136%': '136px',
+        'person': '75vh',
       },
       colors:{
         primaryColor: '#57697D',
